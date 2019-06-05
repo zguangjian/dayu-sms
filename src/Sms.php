c375e658b875d2c2e7591017bbc120fa0e1c477c
TheGJian <TheGJian@outlook.com>
2019-06-05 18:32:30 +0800

    file

---
+++ src/Sms.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace ZGuangJian;
+
+/**
+ * 
+ */
+class Sms 
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+
+
+	public function encode($str)
+    {
+        $res = urlencode($str);
+        $res = preg_replace("/\+/", "%20", $res);
+        $res = preg_replace("/\*/", "%2A", $res);
+        $res = preg_replace("/%7E/", "~", $res);
+        return $res;
+    }
+    /**
+     * 请求url
+     * @param  [type] $url [description]
+     * @return [type]      [description]
+     */
+	public function fetchContent($url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            "x-sdk-client" => "php/2.0.0"
+        ));
+
+        if(substr($url, 0,5) == 'https') {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        }
+
+        $rtn = curl_exec($ch);
+
+        if($rtn === false) {
+            trigger_error("[CURL_" . curl_errno($ch) . "]: " . curl_error($ch), E_USER_ERROR);
+        }
+        curl_close($ch);
+
+        return $rtn;
+    }
+}
